38e2c5e5198c8bf23f01cd73cb6207e5bf302125
Dhifan <dhifanfadhilahaditya@gmail.com>
2026-04-29 08:42:16 +0700

    terraform setup iac

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+ cluster_name = var.clusterName  
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.projectName}-vpc"
+  cidr = "172.16.0.0/16"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
