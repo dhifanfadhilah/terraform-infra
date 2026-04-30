432d11c3a40a12c4e19600411889f92cae5b7c08
Dhifan <dhifanfadhilahaditya@gmail.com>
2026-04-30 08:03:27 +0700

    add nginx ingress controller installation

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,11 +2,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
-  cluster_name = local.cluster_name
+  cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
@@ -15,21 +15,21 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-        name = "node-group-one"
-        instance_types = ["t3.small"]
+      name           = "node-group-one"
+      instance_types = ["t3.small"]
 
-        min_size = 1
-        max_size = 2
-        desired_size = 1
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
 
     two = {
-        name = "node-group-two"
-        instance_types = ["t3.small"]
+      name           = "node-group-two"
+      instance_types = ["t3.small"]
 
-        min_size = 1
-        max_size = 3
-        desired_size = 2
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 locals {
- cluster_name = var.clusterName  
+  cluster_name = var.clusterName
 }
 
 provider "aws" {
@@ -9,6 +9,25 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host = module.eks.cluster_endpoint
+  host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.clusterName]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.clusterName]
+      command     = "aws"
+    }
+  }
 }
